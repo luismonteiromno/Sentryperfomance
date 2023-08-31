6ed4cd078464839ea029790112f923f04e734d79
Luis Monteiro <felipemoreno9065@gmail.com>
2023-08-30 23:22:30 -0300

    New fields of address added

--- library/models.py
+++ library/models.py
@@ -7,6 +7,9 @@ class Librarys(models.Model):
     owner_library = models.ForeignKey(Users, verbose_name='Dono', related_name='owner_library', on_delete=models.CASCADE, null=True)
     name = models.CharField('Nome da livraria', max_length=100)
     address = models.CharField('Endereço', max_length=50)
+    street = models.CharField('Rua', max_length=50)
+    number = models.CharField('Número', max_length=50)
+    cep = models.CharField('Cep', max_length=12)
     partner_companies = models.ManyToManyField(Companys, verbose_name='Companhias parceiras', related_name='library_partner_companies')
 
     def __str__(self):
