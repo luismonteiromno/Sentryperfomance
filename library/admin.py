69102ad029614560236d7571cfccaeef9bf5d721
Luis Monteiro <felipemoreno9065@gmail.com>
2023-08-30 23:22:51 -0300

    Fields of address added in admin

--- library/admin.py
+++ library/admin.py
@@ -4,7 +4,9 @@ from .models import Librarys
 
 class LibraryAdmin(admin.ModelAdmin):
     fieldsets = (
-        ('Informações da Biblioteca', {'fields': ('owner_library', 'name', 'address', 'partner_companies')}),
+        ('Informações da Biblioteca', {'fields': ('owner_library', 'name', 'partner_companies')}),
+        ('Localização da Biblioteca', {'fields': ('address', 'street', 'number', 'cep')}),
+
     )
     filter_horizontal = ['partner_companies']
 
