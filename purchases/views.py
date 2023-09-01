db423972274e2ce6133adcfbe0600aaf907378b1
Luis Monteiro <felipemoreno9065@gmail.com>
2023-09-01 13:39:31 -0300

    Endpoints of purchases and listing created

--- purchases/views.py
+++ purchases/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,6 +8,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 
 import sentry_sdk
 
+from adminUsibras.models import Books
 from .models import BooksPurchases
 from .serializers import BooksPurchasesSerializers
 
@@ -16,13 +18,54 @@ class BooksPurchasesViewSet(ModelViewSet):
     serializer_class = BooksPurchasesSerializers
     permission_classes = IsAuthenticated
 
+    @action(detail=False, methods=['POST'], permission_classes=[IsAuthenticated])
+    def create_purchase(self, request):
+        user = request.user
+        data = request.data
+        try:
+            now = datetime.now()
+            books = BooksPurchases.objects.create(
+                user_id=user.id,
+                date=now
+            )
+            books.books.add(data['books_id'])
+            return Response({'message': 'Compra feita com sucesso'}, status=status.HTTP_200_OK)
+        except Exception as error:
+            sentry_sdk.capture_exception(error)
+            return Response({'message': 'Erro ao listar todos o livros comprados pelo usuário'},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
     @action(detail=False, methods=['GET'], permission_classes=[IsAuthenticated])
     def books_purchase_by_user(self, request):
         user = request.user
         try:
-            books = BooksPurchases.objects.filter(user=user)
+            books = BooksPurchases.objects.filter(user=user).order_by('date')
             serializer = BooksPurchasesSerializers(books, many=True)
             return Response({'message': 'Sucesso', 'books': serializer.data}, status=status.HTTP_200_OK)
         except Exception as error:
             sentry_sdk.capture_exception(error)
             return Response({'message': 'Erro ao listar todos o livros comprados pelo usuário'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @action(detail=False, methods=['GET'], permission_classes=[IsAuthenticated])
+    def list_all_purchases(self, request):
+        try:
+            books = BooksPurchases.objects.all().order_by('date')
+            serializer = BooksPurchasesSerializers(books, many=True)
+            return Response({'message': 'Sucesso', 'books': serializer.data}, status=status.HTTP_200_OK)
+        except Exception as error:
+            sentry_sdk.capture_exception(error)
+            return Response({'message': 'Erro ao listar todos o livros comprados pelo usuário'},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @action(detail=False, methods=['GET'], permission_classes=[IsAuthenticated])
+    def purchase_by_id(self, request):
+        params = request.query_params
+        try:
+            purchase = BooksPurchases.objects.get(pk=params['purchase_id'])
+            serializer = BooksPurchasesSerializers(purchase)
+            return Response({'message': 'Sucesso', 'books': serializer.data}, status=status.HTTP_200_OK)
+        except Exception as error:
+            sentry_sdk.capture_exception(error)
+            return Response({'message': 'Erro ao listar todos o livros comprados pelo usuário'},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
