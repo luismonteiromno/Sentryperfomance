4b88d861155cd31735ec6caa84eefa59da344614
Luis Monteiro <felipemoreno9065@gmail.com>
2023-10-27 23:45:32 -0300

    Routers of API for 'about_us' and 'terms_of_use' created

--- usibras_admin/urls.py
+++ usibras_admin/urls.py
@@ -21,6 +21,7 @@ from rest_framework import routers
 from adminUsibras.views import BooksViewSet, CompanysViewSet
 from library.views import LibraryViewSet
 from purchases.views import BooksPurchasesViewSet
+from about_us.views import AboutUsViewSet, TermsOfUseViewSet
 
 router = routers.DefaultRouter()
 # from admin_notification.views import check_notification_view
@@ -30,6 +31,9 @@ router.register(r'books', BooksViewSet, basename="Books")
 router.register(r'companys', CompanysViewSet, basename='companys')
 router.register(r'librarys', LibraryViewSet, basename='library')
 router.register(r'books_purchase', BooksPurchasesViewSet, basename='books_purchase')
+router.register(r'about_us', AboutUsViewSet, basename='books_purchase')
+router.register(r'terms_of_use', TermsOfUseViewSet, basename='books_purchase')
+
 
 admin.site.site_title = 'API - BOOKS'
 admin.site.site_header = 'BOOKS - API'
