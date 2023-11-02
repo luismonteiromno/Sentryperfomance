94eeda49adceac6f3e2d6ee2d1615c48d629bf32
Luis Monteiro <felipemoreno9065@gmail.com>
2023-11-02 16:23:03 -0300

    Validation of fields minimum and maximum delivery created

--- library/admin.py
+++ library/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Librarys
+from django import forms
+
+
+class FormLibrary(forms.ModelForm):
+    def clean(self):
+        cleaned_data = super().clean()
+        delivery = cleaned_data.get('delivery')
+        print(delivery)
+        minimum_delivery = cleaned_data.get('minimum_delivery')
+        maximum_delivery = cleaned_data.get('maximum_delivery')
+
+        if delivery == True and minimum_delivery == None and maximum_delivery == None:
+            raise forms.ValidationError('Preencha os campos de "tempo minímo de entrega" e "tempo máximo de entrega"!')
+
+        if minimum_delivery != None and maximum_delivery != None and minimum_delivery >= maximum_delivery:
+            raise forms.ValidationError('O tempo minimo de entrega não pode ser menor/igual ao tempo máximo!')
 
 
 class LibraryAdmin(admin.ModelAdmin):
@@ -8,6 +24,7 @@ class LibraryAdmin(admin.ModelAdmin):
         ('Localização da Biblioteca', {'fields': ('address', 'street', 'number', 'cep')}),
         ('Entrega', {'fields': ('delivery', 'minimum_delivery', 'maximum_delivery')})
     )
+    form = FormLibrary
     filter_horizontal = ['partner_companies']
     list_display = ['id', 'name']
 
