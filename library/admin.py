f5aa6e2d1437ca792a6d97b883c5a0b961026648
Luis Monteiro <felipemoreno9065@gmail.com>
2023-11-01 23:14:08 -0300

    Field 'delivery' added in library admin

--- library/admin.py
+++ library/admin.py
@@ -6,7 +6,7 @@ class LibraryAdmin(admin.ModelAdmin):
     fieldsets = (
         ('Informações da Biblioteca', {'fields': ('owner_library', 'name', 'partner_companies')}),
         ('Localização da Biblioteca', {'fields': ('address', 'street', 'number', 'cep')}),
-
+        ('Entrega', {'fields': ('delivery', 'minimum_delivery', 'maximum_delivery')})
     )
     filter_horizontal = ['partner_companies']
 
