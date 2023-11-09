bdeaa0f95628834fcbd017b0c6e9cbd2bf3cad24
Luis Monteiro <felipemoreno9065@gmail.com>
2023-11-08 22:49:04 -0300

    PrivacyPolice router created

--- usibras_admin/routers.py
+++ usibras_admin/routers.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 from adminUsibras.views import BooksViewSet, CompanysViewSet
 from library.views import LibraryViewSet
 from purchases.views import BooksPurchasesViewSet
-from about_us.views import AboutUsViewSet, TermsOfUseViewSet
+from about_us.views import AboutUsViewSet, TermsOfUseViewSet, PrivacyPoliceViewSet
 
 router = routers.DefaultRouter()
 
@@ -10,5 +10,6 @@ router.register(r'books', BooksViewSet, basename="Books")
 router.register(r'companys', CompanysViewSet, basename='companys')
 router.register(r'librarys', LibraryViewSet, basename='library')
 router.register(r'books_purchase', BooksPurchasesViewSet, basename='books_purchase')
-router.register(r'about_us', AboutUsViewSet, basename='books_purchase')
-router.register(r'terms_of_use', TermsOfUseViewSet, basename='books_purchase')
+router.register(r'about_us', AboutUsViewSet, basename='about_us')
+router.register(r'terms_of_use', TermsOfUseViewSet, basename='terms_of_use')
+router.register(r'privacy_police', PrivacyPoliceViewSet, basename='privacy_police')
