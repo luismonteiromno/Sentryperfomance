7a340636fad708a3a7d93dd2d713325b4e00e59c
Luis Monteiro <felipemoreno9065@gmail.com>
2023-11-23 23:32:21 -0300

    New users ViewSet

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
+from .models import Users
