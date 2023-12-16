ff376b0a724915e75e35b24be02ed9ee8f2c2ede
Luis Monteiro <felipemoreno9065@gmail.com>
2023-12-15 23:00:58 -0300

    Fixed redefine password user

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from .models import Users
+from django.contrib.auth.forms import UserChangeForm
+
+
+class UserForm(UserChangeForm):
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
 
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ['id', 'username', 'email', 'company_owner', 'library_owner']
     list_display_links = ['id', 'username']
+    form = UserForm
 
 
 admin.site.register(Users, UserAdmin)
