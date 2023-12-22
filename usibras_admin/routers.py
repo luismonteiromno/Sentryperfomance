a8037e6516413e03aa2ffbe8df82dfcf5b2b92fa
Luis Monteiro <felipemoreno9065@gmail.com>
2023-12-21 22:14:41 -0300

    Route of adverts in API registered

--- usibras_admin/routers.py
+++ usibras_admin/routers.py
@@ -5,6 +5,7 @@ from library.views import LibraryViewSet
 from purchases.views import BooksPurchasesViewSet
 from about_us.views import AboutUsViewSet, TermsOfUseViewSet, PrivacyPoliceViewSet
 from payment_methods.views import PaymentMethodsViewSet
+from adverts.views import AdvertsViewSet
 
 router = routers.DefaultRouter()
 
@@ -18,3 +19,4 @@ router.register(r'terms_of_use', TermsOfUseViewSet, basename='terms_of_use')
 router.register(r'privacy_police', PrivacyPoliceViewSet, basename='privacy_police')
 router.register(r'payment_methods', PaymentMethodsViewSet, basename='payment_methods')
 router.register(r'books_genres', BooksGenreViewSet, basename='books_genres')
+router.register(r'adverts', AdvertsViewSet, basename='adverts')
