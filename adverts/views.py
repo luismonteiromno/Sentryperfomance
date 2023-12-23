96167038222845dc648b0c5f5a0bb9657dbc5931
Luis Monteiro <felipemoreno9065@gmail.com>
2023-12-22 22:34:49 -0300

    Endpoints finished

--- adverts/views.py
+++ adverts/views.py
@@ -26,6 +26,9 @@ class AdvertsViewSet(ModelViewSet):
             if user in library.owner_library.all():
                 create_at = datetime.strptime(data['create_at'], '%d/%m/%Y %H:%M')
                 expiration = datetime.strptime(data['expiration'], '%d/%m/%Y %H:%M')
+                if create_at >= expiration:
+                    return Response({'message': 'A data de criação do anúncio não pode ser maior/igual a data de expiração!'},
+                                    status=status.HTTP_400_BAD_REQUEST)
                 Adverts.objects.create(
                     announcement=data['announcement'],
                     library_id=library.id,
@@ -51,4 +54,63 @@ class AdvertsViewSet(ModelViewSet):
             return Response({'message': 'Anúncios encontrados', 'adverts': serializer.data}, status=status.HTTP_200_OK)
         except Exception as error:
             print(error)
-            return Response({'message': 'Erro ao criar anúncio!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({'message': 'Erro ao listar anúncios!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @action(detail=False, methods=['PATCH'], permission_classes=[IsAuthenticated])
+    def update_announcement(self, request):
+        user = request.user
+        data = request.data
+        try:
+            update_announcement = Adverts.objects.get(id=data['announcement_id'])
+            if user in update_announcement.library.owner_library.all():
+                create_at = datetime.strptime(data['create_at'], '%d/%m/%Y %H:%M')
+                expiration = datetime.strptime(data['expiration'], '%d/%m/%Y %H:%M')
+                if create_at >= expiration:
+                    return Response({'message': 'A data de criação do anúncio não pode ser maior/igual a data de expiração!'},
+                                    status=status.HTTP_400_BAD_REQUEST)
+                update_announcement.announcement = data['announcement']
+                update_announcement.create_at = create_at
+                update_announcement.expiration = expiration
+                update_announcement.save()
+                return Response({'message': 'Anúncio atualizado com sucesso'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'message': 'Somente o(s) usuário(s) dono(s) desta biblioteca pode(m) atualizar anúncios!'},
+                                status=status.HTTP_403_FORBIDDEN)
+        except ObjectDoesNotExist:
+            return Response({'message': 'Anúncio não encontrado!'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as error:
+            print(error)
+            return Response({'message': 'Erro ao atualizar anúncio!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @action(detail=False, methods=['GET'], permission_classes=[IsAuthenticated])
+    def advert_by_id(self, request):
+        params = request.query_params
+        now = datetime.now()
+        try:
+            advert = Adverts.objects.get(id=params['advert_id'], create_at__lte=now, expiration__gte=now)
+            serializer = AdvertsSerializers(advert)
+            return Response({'message': 'Anúncio encontrado', 'advert': serializer.data}, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response({'message': 'Anúncio não encontrado!'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as error:
+            print(error)
+            return Response({'message': 'Erro ao listar anúncio!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @action(detail=False, methods=['DELETE'], permission_classes=[IsAuthenticated])
+    def delete_advert(self, request):
+        params = request.query_params
+        user = request.user
+        now = datetime.now()
+        try:
+            advert = Adverts.objects.get(id=params['advert_id'], create_at__lte=now, expiration__gte=now)
+            if user not in advert.library.owner_library.all():
+                return Response({'message': 'Somente o(s) usuário(s) dono(s) desta biblioteca pode(m) atualizar anúncios!'},
+                    status=status.HTTP_403_FORBIDDEN)
+            else:
+                advert.delete()
+                return Response({'message': 'Anúncio deletado com sucesso'}, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response({'message': 'Anúncio não encontrado!'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as error:
+            print(error)
+            return Response({'message': 'Erro ao listar anúncio!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
