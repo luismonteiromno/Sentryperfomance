2c2ba3be32023455f516ab339b0f6244f42e0b37
Luis Monteiro <felipemoreno9065@gmail.com>
2023-12-25 22:43:41 -0300

    New table of books adverts

--- adverts/models.py
+++ adverts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from adminUsibras.models import Books
 from library.models import Librarys
 from users.models import Users
 
@@ -27,5 +28,20 @@ class AdvertsViewed(models.Model):
         return str(f'{self.announcement} - {self.user_viewed}')
 
     class Meta:
-        verbose_name = 'Anúncio visualizado'
-        verbose_name_plural = 'Anúncios visualizados'
+        verbose_name = 'Anúncio Visualizado'
+        verbose_name_plural = 'Anúncios Visualizados'
+
+
+class AdvertsBooks(models.Model):
+    announcement = models.ImageField('Anúncio')
+    book = models.ForeignKey(Books, verbose_name='Anúncio do Livro', on_delete=models.CASCADE)
+    description = models.TextField('Descrição do Anúncio', max_length=320, default='')
+    create_at = models.DateTimeField('Criado em', auto_now_add=True)
+    expiration = models.DateTimeField('Expira em', auto_now_add=True)
+
+    def __str__(self):
+        return str(f"{self.announcement} - {self.book}")
+
+    class Meta:
+        verbose_name = 'Anúncio de Livros'
+        verbose_name_plural = 'Anúncios de Livros'
