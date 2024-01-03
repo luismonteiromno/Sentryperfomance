6cccc0c74e386bc543bcf1d5f03e04165616af39
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-02 23:57:23 -0300

    New table of adverts books viewed

--- adverts/models.py
+++ adverts/models.py
@@ -45,3 +45,9 @@ class AdvertsBooks(models.Model):
     class Meta:
         verbose_name = 'Anúncio de Livros'
         verbose_name_plural = 'Anúncios de Livros'
+
+
+class AdvertsBookViewed(models.Model):
+    user_viewed = models.ForeignKey(Users, verbose_name='Visualizado pelo usuário', related_name='user_viewed_book_advert', on_delete=models.CASCADE)
+    announcement = models.ForeignKey(AdvertsBooks, verbose_name='Anúncio visualizado', related_name='advert_book_viewed', on_delete=models.CASCADE)
+    date = models.DateTimeField('Data da visualização', auto_now_add=True)
